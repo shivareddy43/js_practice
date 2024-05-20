16a47a4bf43dbad8af9ee9138f16e9ec87a0bdd9
Shiva Rajamuri Reddy <rshiva@synopsys.com>
2024-05-20 22:41:46 +0530

    Added conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33";
+
+console.log(typeof score);//string
+let valueIntNumber = Number(score);
+console.log(typeof valueIntNumber);// number
+
+score = "33acd";
+
+console.log(typeof score);//string
+valueIntNumber = Number(score);
+console.log(typeof valueIntNumber);// number
+console.log(valueIntNumber);//NaN
+
+// "33"= > 33
+// "33sjbf" => NaN
+// true -> 1;false->0
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);//true
+
+
+// 1-> true,0->false
+//""-=> false,"trt"-> true
+// Number,Boolear,String -> objects
