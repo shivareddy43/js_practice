28f3ea29141b753edc29cea2ca6c0358bbc3f8b7
Shiva Rajamuri Reddy <rshiva@synopsys.com>
2024-05-20 22:31:22 +0530

    Added datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version
+
+let name="shiva"
+let age = 18
+let isLoggedIn = false
+
+// null -> standalone value,true/false,string,number,undefined
+//symbol -> unique
+// object
+
+console.log(typeof age);//number
+console.log(typeof null);//object
