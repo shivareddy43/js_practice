3083ad9dd828cc4e1e0412fa992571b504939981
Shiva Rajamuri Reddy <rshiva@synopsys.com>
2024-06-04 17:40:23 +0530

    Added for loops

---
+++ 04_control_tatements/01.js
@@ -0,0 +1,12 @@
+const array = ['a', 'b', 'c'];
+for (const value of array) {
+    console.log(value);
+}
+const obj = { a: 1, b: 2, c: 3 };
+for (const key in obj) {
+    console.log(key, obj[key]);
+}
+const array2 = [1, 2, 3, 4, 5];
+for (let i = 0; i < array2.length; i++) {
+    console.log(array2[i]);
+}
