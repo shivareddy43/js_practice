2394b7b2c5ff0b7bfdb18245b625b37b113f0356
Shiva Rajamuri Reddy <rshiva@synopsys.com>
2024-06-04 15:41:29 +0530

    Added function scope

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -10,4 +10,16 @@ console.log(a) // error if 'a' was not declared outside if. And 'a' inside if do
 // console.log(b) // error
 console.log(c) // no error
 
+// console.log(addOne(5)) // this will not cause error
 
+function addOne(num){
+    return num+1
+}
+// console.log(addOne(5)) // this will not cause error
+
+// console.log(addTwo(5)) // this will cause error
+const addTwo = function(num){
+    return num+2
+}
+
+console.log(addTwo(5))
